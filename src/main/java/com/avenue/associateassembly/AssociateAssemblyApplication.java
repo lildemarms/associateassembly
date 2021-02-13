0e22cf2f55dc30ab750956b72e33783c59065db4
lildemarms <lildemar@gmail.com>
2021-02-13 16:51:49 -0300

    first commit

---
+++ src/main/java/com/avenue/associateassembly/AssociateAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.avenue.associateassembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociateAssemblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociateAssemblyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avenue/associateassembly/AssociateAssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avenue.associateassembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociateAssemblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
